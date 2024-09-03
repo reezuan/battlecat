7d4948c2da5854754c0a05a1a072f0d632ba7b96
Rizuan <john.lee.coco@gmail.com>
2024-09-03 13:41:02 +0800

    test: test if required properties are present

--- tests/classes/Ship.test.js
+++ tests/classes/Ship.test.js
@@ -5,3 +5,9 @@ let ship = new Ship();
 test("creates a Ship object", () => {
     expect(ship).toBeInstanceOf(Ship);
 });
+
+test("has required properties", () => {
+    expect(ship).toHaveProperty("length");
+    expect(ship).toHaveProperty("timesHit");
+    expect(ship).toHaveProperty("isSunk");
+});
