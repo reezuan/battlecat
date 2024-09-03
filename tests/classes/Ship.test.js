f05fc1b3e4c354cc855dceadf4bc73c7a9f545c0
Rizuan <john.lee.coco@gmail.com>
2024-09-03 16:04:14 +0800

    test: remove tests for isSunk property
    Remove isSunk property and implement it as a method instead in a future
    commit. Said method, when invoked, will compare timesHit property to
    length property.

--- tests/classes/Ship.test.js
+++ tests/classes/Ship.test.js
@@ -18,14 +18,9 @@ test("creates a Ship object", () => {
 test("has required properties", () => {
     expect(ship).toHaveProperty("length");
     expect(ship).toHaveProperty("timesHit");
-    expect(ship).toHaveProperty("isSunk");
 });
 
 // Test default property values
 test("timesHit property is 0 by default", () => {
     expect(ship.timesHit).toBe(0);
-});
-
-test("isSunk property is false by default", () => {
-    expect(ship.isSunk).not.toBeTruthy();
 });
