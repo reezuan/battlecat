54351030d746725a536daf0695a5b299347b88e8
Rizuan <john.lee.coco@gmail.com>
2024-09-03 16:32:59 +0800

    feat: change how many times a Ship has been hit

--- src/classes/Ship.js
+++ src/classes/Ship.js
@@ -3,6 +3,9 @@ export class Ship {
     constructor(length) {
         this.length = length;
         this.timesHit = 0;
-        this.isSunk = false;
+    }
+
+    hit() {
+        this.timesHit += 1;
     }
 }
