2d04ab1b815a5296ab07c1640676fe3739c9d072
Rizuan <john.lee.coco@gmail.com>
2024-09-03 15:49:43 +0800

    test: add tests to check object default properties & truthiness

--- tests/classes/Ship.test.js
+++ tests/classes/Ship.test.js
@@ -2,6 +2,8 @@ import { Ship } from "../../src/classes/Ship";
 
 let ship = new Ship();
 
+// OBJECT CONSTRUCTOR
+
 test("constructor returns a defined object", () => {
     expect(ship).toBeDefined();
 });
@@ -10,8 +12,20 @@ test("creates a Ship object", () => {
     expect(ship).toBeInstanceOf(Ship);
 });
 
+// OBJECT PROPERTIES
+
+// Test presence of properties
 test("has required properties", () => {
     expect(ship).toHaveProperty("length");
     expect(ship).toHaveProperty("timesHit");
     expect(ship).toHaveProperty("isSunk");
+});
+
+// Test default property values
+test("timesHit property is 0 by default", () => {
+    expect(ship.timesHit).toBe(0);
+});
+
+test("isSunk property is false by default", () => {
+    expect(ship.isSunk).not.toBeTruthy();
 });
