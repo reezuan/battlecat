bbea125f29430de43041494b6f9bb483a05f9816
Rizuan <john.lee.coco@gmail.com>
2024-11-10 09:50:05 +0800

    build: update webpack config, add template.html

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,22 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
-    devtool: 'inline-source-map',
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Battlecat',
-        }),
-    ],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/template.html',
+            title: 'Battlecat',
+        }),
+    ],
+    devtool: 'eval-source-map',
+    devServer: {
+        watchFiles: ['./src/template.html'],
     },
     module: {
         rules: [
@@ -26,6 +32,10 @@ module.exports = {
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
                 type: 'asset/resource',
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
             },
         ],
     },
