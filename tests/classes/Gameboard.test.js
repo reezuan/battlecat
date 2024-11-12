28185d2d1e0751e3dea4b61fb5826ffcf5618568
Rizuan <john.lee.coco@gmail.com>
2024-11-12 23:57:39 +0800

    test: add tests for 'board' property

--- tests/classes/Gameboard.test.js
+++ tests/classes/Gameboard.test.js
@@ -11,3 +11,25 @@ test("constructor returns a defined object", () => {
 test("creates a Gameboard object", () => {
     expect(gameboard).toBeInstanceOf(Gameboard);
 });
+
+// OBJECT PROPERTIES
+
+test("'board' property is defined", () => {
+    expect(gameboard.board).toBeDefined();
+});
+
+test("'board' property is an object", () => {
+    expect(gameboard.board).toBeInstanceOf(Object);
+});
+
+test("each value in 'board' object has required properties and correct pre-defined values", () => {
+    for (const property in gameboard.board) {
+        let currentRow = property.substring(1);
+        let currentColumn = property.substring(0, 1);
+        
+        expect(gameboard.board[property]).toHaveProperty("row", currentRow);
+        expect(gameboard.board[property]).toHaveProperty("column", currentColumn);
+        expect(gameboard.board[property]).toHaveProperty("isHit", false);
+        expect(gameboard.board[property]).toHaveProperty("occupiedBy", null);
+    }
+});
