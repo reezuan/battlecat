937cd9f3c0df84f203696b0e93d80b92e34bba1d
Rizuan <john.lee.coco@gmail.com>
2024-11-16 15:33:00 +0800

    refactor: instantiate new gameboard object in each test
    Instead of instantiating a Gameboard object in the global scope,
    instantiated one in each test to help ensure each test is isolated from
    the others.

--- tests/classes/Gameboard.test.js
+++ tests/classes/Gameboard.test.js
@@ -1,15 +1,19 @@
 import { Gameboard } from "../../src/classes/Gameboard.js";
 import { Ship } from "../../src/classes/Ship.js";
 
+afterEach(() => {
+    jest.restoreAllMocks();
+});
+
 // -------------------- OBJECT CONSTRUCTOR --------------------
 
-let gameboard = new Gameboard();
-
 test("constructor returns a defined object", () => {
+    const gameboard = new Gameboard();
     expect(gameboard).toBeDefined();
 });
 
 test("creates a Gameboard object", () => {
+    const gameboard = new Gameboard();
     expect(gameboard).toBeInstanceOf(Gameboard);
 });
 
@@ -18,14 +22,18 @@ test("creates a Gameboard object", () => {
 // ----- Gameboard.board -----
 
 test("'board' property is defined", () => {
+    const gameboard = new Gameboard();
     expect(gameboard.board).toBeDefined();
 });
 
 test("'board' property is an object", () => {
+    const gameboard = new Gameboard();
     expect(gameboard.board).toBeInstanceOf(Object);
 });
 
 test("each value in 'board' object has required properties and correct pre-defined values", () => {
+    const gameboard = new Gameboard();
+
     for (const property in gameboard.board) {
         let currentRow = property.substring(1);
         let currentColumn = property.substring(0, 1);
@@ -40,18 +48,23 @@ test("each value in 'board' object has required properties and correct pre-defin
 // ----- Gameboard.placedShips -----
 
 test("Gameboard.placedShips is defined", () => {
+    const gameboard = new Gameboard();
     expect(gameboard.placedShips).toBeDefined();
 });
 
 test("Gameboard.placedShips is an object", () => {
+    const gameboard = new Gameboard();
     expect(gameboard.placedShips).toBeInstanceOf(Object);
 });
 
 test("Gameboard.placedShips has exactly 5 properties", () => {
+    const gameboard = new Gameboard();
     expect(Object.keys(gameboard.placedShips).length).toBe(5);
 });
 
 test("each property in Gameboard.placedShips has correct names", () => {
+    const gameboard = new Gameboard();
+
     expect(gameboard.placedShips).toHaveProperty("carrier");
     expect(gameboard.placedShips).toHaveProperty("battleship");
     expect(gameboard.placedShips).toHaveProperty("cruiser");
@@ -60,6 +73,8 @@ test("each property in Gameboard.placedShips has correct names", () => {
 });
 
 test("each object in Gameboard.placedShips has 'ship' and 'spaceRequired' properties", () => {
+    const gameboard = new Gameboard();
+    
     for (const ship in gameboard.placedShips) {
         expect(gameboard.placedShips[ship]).toHaveProperty("ship");
         expect(gameboard.placedShips[ship]).toHaveProperty("spaceRequired");
@@ -67,12 +82,16 @@ test("each object in Gameboard.placedShips has 'ship' and 'spaceRequired' proper
 });
 
 test("Gameboard.placedShips.ship is null when initialised", () => {
+    const gameboard = new Gameboard();
+
     for (const ship in gameboard.placedShips) {
         expect(gameboard.placedShips[ship].ship).toBeNull();
     }
 });
 
 test("Gameboard.placedShips.spaceRequired has correct value when initialised", () => {
+    const gameboard = new Gameboard();
+    
     expect(gameboard.placedShips.carrier.spaceRequired).toBe(5);
     expect(gameboard.placedShips.battleship.spaceRequired).toBe(4);
     expect(gameboard.placedShips.cruiser.spaceRequired).toBe(3);
@@ -81,6 +100,8 @@ test("Gameboard.placedShips.spaceRequired has correct value when initialised", (
 });
 
 test("Gameboard.placedShips.ship is not null after a Ship object is assigned to it", () => {
+    const gameboard = new Gameboard();
+
     let carrier = new Ship(5);
     let battleship = new Ship(4);
     let cruiser = new Ship(3);
@@ -99,6 +120,20 @@ test("Gameboard.placedShips.ship is not null after a Ship object is assigned to 
 });
 
 test("Gameboard.placedShips.spaceRequired must match the length of the Ship object assigned to Gameboard.placedShips.ship", () => {
+    const gameboard = new Gameboard();
+
+    let carrier = new Ship(5);
+    let battleship = new Ship(4);
+    let cruiser = new Ship(3);
+    let submarine = new Ship(3);
+    let destroyer = new Ship(2);
+
+    gameboard.placeShip(carrier, ["A1", "A2", "A3", "A4", "A5"]);
+    gameboard.placeShip(battleship, ["B1", "B2", "B3", "B4"]);
+    gameboard.placeShip(cruiser, ["C1", "C2", "C3"]);
+    gameboard.placeShip(submarine, ["D1", "D2", "D3"]);
+    gameboard.placeShip(destroyer, ["E1", "E2"]);
+    
     for (const ship in gameboard.placedShips) {
         expect(gameboard.placedShips[ship].spaceRequired).toBe(gameboard.placedShips[ship].ship.length);
     }
