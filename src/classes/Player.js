2b343b849ea5f5ad7f11535eea67efa7695c3ed6
Rizuan <john.lee.coco@gmail.com>
2024-11-17 13:11:03 +0800

    feat: create Player class

---
+++ src/classes/Player.js
@@ -0,0 +1,11 @@
+export class Player {
+    
+    /**
+     * @param {string} name The name of the player.
+     * @param {boolean} isComputerControlled True if the player is computer-controlled, false otherwise.
+     */
+    constructor(name, isComputerControlled) {
+        this.name = name;
+        this.isComputerControlled = isComputerControlled;
+    }
+}
