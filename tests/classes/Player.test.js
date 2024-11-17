d4313933086101ae1f16d1a2c7c82e75a5fab83a
Rizuan <john.lee.coco@gmail.com>
2024-11-17 13:18:42 +0800

    test: add tests for Player.name property

--- tests/classes/Player.test.js
+++ tests/classes/Player.test.js
@@ -11,3 +11,22 @@ test("creates a Player object", () => {
     const player = new Player("Player 1", false);
     expect(player).toBeInstanceOf(Player);
 });
+
+// -------------------- OBJECT PROPERTIES --------------------
+
+// ----- Player.name -----
+
+test("Player.name property is defined", () => {
+    const player = new Player("Player 1", false);
+    expect(player.name).toBeDefined();
+});
+
+test("Player.name property is a string", () => {
+    const player = new Player("Player 1", false);
+    expect(typeof player.name).toBe("string");
+});
+
+test("Player.name property has the correct value", () => {
+    const player = new Player("Player 1", false);
+    expect(player.name).toBe("Player 1");
+});
