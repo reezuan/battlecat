d17c3136c92d45c692e23b5d6833053fe31cbdf5
Rizuan <john.lee.coco@gmail.com>
2024-11-20 17:09:31 +0800

    feat: enable players to input custom player name

---
+++ src/components/NameInput.js
@@ -0,0 +1,23 @@
+export function NameInput() {
+    const form = document.createElement("form");
+    form.setAttribute("action", "");
+    form.classList.add("name-input");
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+    });
+
+    // Text input
+    const input = document.createElement("input"); 
+    
+    input.setAttribute("type", "text");
+    input.setAttribute("required", "");
+    input.setAttribute("id", "player-name");
+    input.setAttribute("minlength", "1");
+    input.setAttribute("maxlength", "20");
+
+    // Assemble the component
+    form.appendChild(input);
+
+    return form;
+}
