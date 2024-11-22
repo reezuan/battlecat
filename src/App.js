48d3c34a703d850eef5dbf227ad74728c347a336
Rizuan <john.lee.coco@gmail.com>
2024-11-22 15:31:27 +0800

    feat: validate user fills in required player names

--- src/App.js
+++ src/App.js
@@ -17,5 +17,56 @@ export function App() {
     app.appendChild(GameOptionsContainer());
     app.appendChild(Footer());
 
+    const startGameButton = app.querySelector("button.start-game");
+
+    startGameButton.addEventListener("click", () => {
+        const playerOneNameInput = document.querySelector("[data-player=\"player-one\"] input");
+        const playerTwoNameInput = document.querySelector("[data-player=\"player-two\"] input");
+        const isPlayerTwoHuman = document.querySelector("#playerToggle").checked;
+
+        if (isPlayerTwoHuman) {
+            if (playerOneNameInput.value === "" && playerTwoNameInput.value === "") {
+                document.querySelector("button.start-game").textContent = "Fill in player names!";
+            
+                setTimeout(() => {
+                    document.querySelector("button.start-game").textContent = "Start game";
+                }, 1500);
+            
+                return;
+            } else if (playerOneNameInput.value !== "" && playerTwoNameInput.value === "") {
+                document.querySelector("button.start-game").textContent = "Fill in Player 2 name!";
+            
+                setTimeout(() => {
+                    document.querySelector("button.start-game").textContent = "Start game";
+                }, 1500);
+            
+                return;
+            } else if (playerOneNameInput.value === "" && playerTwoNameInput.value !== "") {
+                document.querySelector("button.start-game").textContent = "Fill in Player 1 name!";
+            
+                setTimeout(() => {
+                    document.querySelector("button.start-game").textContent = "Start game";
+                }, 1500);
+            
+                return;
+            }
+        } else {
+            if (playerOneNameInput.value === "") {
+                document.querySelector("button.start-game").textContent = "Fill in Player 1 name!";
+                
+                setTimeout(() => {
+                    document.querySelector("button.start-game").textContent = "Start game";
+                  }, 1500);
+                
+                return;
+            }
+        }
+    });
+
+    // 2) When the player clicks the "Start Game" button, fade out and eventually
+    // remove the introduction screen. Then, show the gameboards.
+
+    // MAIN GAME SCREEN
+
     return app;
 }
