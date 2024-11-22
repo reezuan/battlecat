0d53c1a64ddea703490b25597d4e88afab3f44ed
Rizuan <john.lee.coco@gmail.com>
2024-11-22 13:53:38 +0800

    feat: add container element for player name input

---
+++ src/components/NameInputContainer.js
@@ -0,0 +1,27 @@
+import { NameInput } from "./NameInput.js";
+import AdmiralOiia from "../assets/admiral-oiia.png";
+import AdmiralMaxwell from "../assets/admiral-maxwell.png";
+
+/**
+ * @param {boolean} isPlayerOne 
+ * @returns 
+ */
+export function NameInputContainer(isPlayerOne) {
+    const container = document.createElement("div");
+    container.classList.add("name-input");
+    container.setAttribute("data-player", isPlayerOne ? "player-one" : "player-two");
+
+    const playerImage = new Image();
+    playerImage.src = isPlayerOne ? AdmiralOiia : AdmiralMaxwell;
+    
+    const namePrompt = document.createElement("span");
+    namePrompt.textContent = `Enter Player ${ isPlayerOne ? "1" : "2" } name:`;
+    
+    const playerNameInput = NameInput();
+    
+    container.appendChild(playerImage);
+    container.appendChild(namePrompt);
+    container.appendChild(playerNameInput);
+
+    return container;
+}
